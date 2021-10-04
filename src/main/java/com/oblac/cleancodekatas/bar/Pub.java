007eb860febbc1c9398f45060721619ffabbdf10
Sava Gaborov <sava.gaborov@codetri.be>
2021-10-04 13:21:42 +0200

    rebase

--- src/main/java/com/oblac/cleancodekatas/bar/Pub.java
+++ src/main/java/com/oblac/cleancodekatas/bar/Pub.java
@@ -7,13 +7,12 @@ public class Pub {
     public static final String GT = "gt";
     public static final String BACARDI_SPECIAL = "bacardi_special";
 
-    //TODO interactive rebase #1
+    //TODO interactive rebase REBASE UNDERSTAND
     public int computeCost(String drink, boolean student, int amount) {
         if (amount > 2 && (drink == GT || drink == BACARDI_SPECIAL)) {
             throw new RuntimeException("Too many drinks, max 2.");
         }
 
-        //TODO interactive rebase #2
         int price;
         if (drink.equals(ONE_BEER)) {
             price = 74;
