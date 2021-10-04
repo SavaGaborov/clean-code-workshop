6f333b717654f40bc866b5a3146ac531ad53ed9d
Sava Gaborov <sava.gaborov@codetri.be>
2021-10-04 12:48:57 +0200

    second

--- src/main/java/com/oblac/cleancodekatas/bar/Pub.java
+++ src/main/java/com/oblac/cleancodekatas/bar/Pub.java
@@ -7,7 +7,6 @@ public class Pub {
     public static final String GT = "gt";
     public static final String BACARDI_SPECIAL = "bacardi_special";
 
-    //TODO gi
     public int computeCost(String drink, boolean student, int amount) {
         if (amount > 2 && (drink == GT || drink == BACARDI_SPECIAL)) {
             throw new RuntimeException("Too many drinks, max 2.");
